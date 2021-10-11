6d5fdcc007e5d71f108716308e788e51bbaa9b67
CarolHe <carol.hcxy@gmail.com>
2021-10-11 15:09:53 -0700

    Made Birds Eye Map Static

---
+++ Assets/Scripts/NavigationCore/DelayedRender.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayedRender : MonoBehaviour
+{
+    [SerializeField] GameObject Object;
+    void Awake ()
+    {
+        StartCoroutine (AfterTime ());
+    }
+ 
+    IEnumerator AfterTime ()
+    {
+        yield return new WaitForSeconds(2f);
+        Object.GetComponent<MeshRenderer>().enabled = true;
+    }
+}
